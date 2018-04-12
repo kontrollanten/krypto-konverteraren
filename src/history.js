56f429961101b9c6323d8419eb821e1651f5c5db
kontrollanten <alexander@width.se>
2018-04-12 07:49:49 +0200

    add Google Analytics

--- src/history.js
+++ src/history.js
@@ -1,3 +1,9 @@
 import createBrowserHistory from 'history/createBrowserHistory';
 
-export default createBrowserHistory();
+const history = createBrowserHistory();
+
+history.listen(location => {
+  gtag('config', 'UA-117408065-1', {'page_path': location.pathname + location.search});
+});
+
+export default history;
