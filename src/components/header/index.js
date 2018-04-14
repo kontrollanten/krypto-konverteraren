f014655eaab5e2492a881b68e39b955b06452847
kontrollanten <alexander@width.se>
2018-04-14 14:10:31 +0200

    Header: add logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,11 +2,15 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router';
 import style from './style.less';
 
+import logo from './logo.png';
+
 export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1><Link href="/">K4 Krypto</Link></h1>
+        <h1><Link href="/">
+          <img src={logo} height="28" />
+        </Link></h1>
         <nav>
           <Link href="/">Konvertera valutor</Link>
           <Link href="/las-av-fil">Ladda upp fil</Link>
