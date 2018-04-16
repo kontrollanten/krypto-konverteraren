2bebe30f4b9fc09a6c6619b4c9405aaa58eb1bf7
kontrollanten <alexander@width.se>
2018-04-16 21:25:28 +0200

    routing: scroll to top upon route change

--- src/components/app.js
+++ src/components/app.js
@@ -18,12 +18,8 @@ import BrowserChecker from './browser-checker';
 const { browser } = UAParser();
 
 export default class App extends Component {
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
 	handleRoute = e => {
-		this.currentUrl = e.url;
+    window.scrollTo(0, 0);
 	};
 
 	render() {
