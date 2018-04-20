86b1196d59e3c07b7c7ddac2d415170173d5ba21
kontrollanten <alexander@width.se>
2018-04-20 22:43:25 +0200

    TransformCurrencyNames: refactor

--- src/components/transform-currency-names/index.js
+++ src/components/transform-currency-names/index.js
@@ -40,9 +40,65 @@ export default class TransformCurrencyNames extends Component {
     });
   }
 
+  renderSuggestions(suggestions) {
+    return (
+      <div>
+        <p>Nedan har vi tagit fram förslag på förändring av valutanamnen, gå igenom dem och ändra de som stämmer.</p>
+
+        <Table className={styles.Center}>
+          <tr>
+            <th>Originalvärde</th>
+            <th>Föreslaget värde</th>
+            <th></th>
+          </tr>
+
+          {suggestions
+              .map(match => ({
+                ...match,
+                disabled: this.props.transformed.indexOf(match.original) > -1 || this.props.currentlyTransforming === match.original,
+                label: this.props.currentlyTransforming === match.original ? 'Laddar' : 'Ändra',
+              }))
+              .map(match => (
+                <tr>
+                  <td>{match.original}</td>
+                  <td>{match.suggestion}</td>
+                  <td>
+                    <Button
+                      { ...{ disabled: match.disabled } }
+                      raised
+                      ripple
+                      onClick={this.handleTransformCurrencyName.bind(this, match)}
+                    >
+                      {match.label}
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+        </Table>
+      </div>
+    );
+  }
+
+  renderNonSuggestions(currencyNames) {
+    return (
+      <div>
+        <p>Vi kunde tyvärr inte hitta några förslag för nedanstående valutanamn:</p>
+        <List>
+          {currencyNames.map(currency => (
+            <List.Item>
+              {currency}
+            </List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
+
   render() {
-    const convertSuggestions = this.props.matches.filter(match => !!match.suggestion);
-    const nonMatches = this.props.matches.filter(match => !match.suggestion);
+    const suggestions = this.props.matches.filter(match => !!match.suggestion);
+    const nonSuggestions = this.props.matches
+      .filter(match => !match.suggestion)
+      .map(match => match.original);
 
     return (
       <Dialog ref={this.setElemRef} onAccept={this.props.onClose}>
@@ -50,55 +106,8 @@ export default class TransformCurrencyNames extends Component {
         <Dialog.Body>
           <p>Alla kryptoplånböcker samt växlingssidor tillhandahåller tyvärr inte CSV-filer i maskinärt avläsbara format.</p>
 
-          {!!convertSuggestions.length && (
-            <div>
-              <p>Nedan har vi tagit fram förslag på förändring av valutanamnen, gå igenom dem och ändra de som stämmer.</p>
-
-              <Table className={styles.Center}>
-                <tr>
-                  <th>Originalvärde</th>
-                  <th>Föreslaget värde</th>
-                  <th></th>
-                </tr>
-
-                {convertSuggestions
-                    .map(match => ({
-                      ...match,
-                      disabled: this.props.transformed.indexOf(match.original) > -1 || this.props.currentlyTransforming === match.original,
-                      label: this.props.currentlyTransforming === match.original ? 'Laddar' : 'Ändra',
-                    }))
-                    .map(match => (
-                      <tr>
-                        <td>{match.original}</td>
-                        <td>{match.suggestion}</td>
-                        <td>
-                          <Button
-                            { ...{ disabled: match.disabled } }
-                            raised
-                            ripple
-                            onClick={this.handleTransformCurrencyName.bind(this, match)}
-                          >
-                            {match.label}
-                          </Button>
-                        </td>
-                      </tr>
-                    ))}
-              </Table>
-            </div>
-          )}
-
-          {!!nonMatches.length && (
-            <div>
-              <p>Vi kunde tyvärr inte hitta några förslag för nedanstående valutanamn:</p>
-              <List>
-                {nonMatches.map(match => (
-                  <List.Item>
-                    {match.original}
-                  </List.Item>
-                ))}
-              </List>
-            </div>
-          )}
+          {!!suggestions.length && this.renderSuggestions(suggestions)}
+          {!!nonSuggestions.length && this.renderNonSuggestions(nonSuggestions)}
         </Dialog.Body>
         <Dialog.Footer>
           <Dialog.FooterButton accept>Okej</Dialog.FooterButton>
