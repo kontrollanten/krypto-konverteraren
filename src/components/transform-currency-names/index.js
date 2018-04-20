cd783a5550187c7a732554be3139dc34b3dda40e
kontrollanten <alexander@width.se>
2018-04-20 22:20:41 +0200

    TransformCurrencyNames: display non matches

--- src/components/transform-currency-names/index.js
+++ src/components/transform-currency-names/index.js
@@ -1,4 +1,6 @@
 import { h, Component } from 'preact';
+import List from 'preact-material-components/List';
+import 'preact-material-components/List/style.css';
 import Dialog from 'preact-material-components/Dialog';
 import Button from 'preact-material-components/Button';
 import 'preact-material-components/Button/style.css';
@@ -40,6 +42,7 @@ export default class TransformCurrencyNames extends Component {
 
   render() {
     const convertSuggestions = this.props.matches.filter(match => !!match.suggestion);
+    const nonMatches = this.props.matches.filter(match => !match.suggestion);
 
     return (
       <Dialog ref={this.setElemRef} onAccept={this.props.onClose}>
@@ -47,40 +50,55 @@ export default class TransformCurrencyNames extends Component {
         <Dialog.Body>
           <p>Alla kryptoplånböcker samt växlingssidor tillhandahåller tyvärr inte CSV-filer i maskinärt avläsbara format.</p>
 
-          {convertSuggestions.length && (
-            <p>Nedan har vi tagit fram förslag på förändring av valutanamnen, gå igenom dem och ändra de som stämmer.</p>
+          {!!convertSuggestions.length && (
+            <div>
+              <p>Nedan har vi tagit fram förslag på förändring av valutanamnen, gå igenom dem och ändra de som stämmer.</p>
+
+              <Table className={styles.Center}>
+                <tr>
+                  <th>Originalvärde</th>
+                  <th>Föreslaget värde</th>
+                  <th></th>
+                </tr>
+
+                {convertSuggestions
+                    .map(match => ({
+                      ...match,
+                      disabled: this.props.transformed.indexOf(match.original) > -1 || this.props.currentlyTransforming === match.original,
+                      label: this.props.currentlyTransforming === match.original ? 'Laddar' : 'Ändra',
+                    }))
+                    .map(match => (
+                      <tr>
+                        <td>{match.original}</td>
+                        <td>{match.suggestion}</td>
+                        <td>
+                          <Button
+                            { ...{ disabled: match.disabled } }
+                            raised
+                            ripple
+                            onClick={this.handleTransformCurrencyName.bind(this, match)}
+                          >
+                            {match.label}
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+              </Table>
+            </div>
           )}
 
-          <Table className={styles.Center}>
-            <tr>
-              <th>Originalvärde</th>
-              <th>Föreslaget värde</th>
-              <th></th>
-            </tr>
-
-            {convertSuggestions
-                .map(match => ({
-                  ...match,
-                  disabled: this.props.transformed.indexOf(match.original) > -1 || this.props.currentlyTransforming === match.original,
-                  label: this.props.currentlyTransforming === match.original ? 'Laddar' : 'Ändra',
-                }))
-                .map(match => (
-                  <tr>
-                    <td>{match.original}</td>
-                    <td>{match.suggestion}</td>
-                    <td>
-                      <Button
-                        { ...{ disabled: match.disabled } }
-                        raised
-                        ripple
-                        onClick={this.handleTransformCurrencyName.bind(this, match)}
-                      >
-                        {match.label}
-                      </Button>
-                    </td>
-                  </tr>
+          {!!nonMatches.length && (
+            <div>
+              <p>Vi kunde tyvärr inte hitta några förslag för nedanstående valutanamn:</p>
+              <List>
+                {nonMatches.map(match => (
+                  <List.Item>
+                    {match.original}
+                  </List.Item>
                 ))}
-          </Table>
+              </List>
+            </div>
+          )}
         </Dialog.Body>
         <Dialog.Footer>
           <Dialog.FooterButton accept>Okej</Dialog.FooterButton>
