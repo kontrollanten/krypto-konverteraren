1a4984b213ccdcc078badf7a65930499be8fa2c9
kontrollanten <alexander@width.se>
2018-04-20 22:11:41 +0200

    TransformCurrencyNames: add desc text and fix styling

--- src/components/transform-currency-names/index.js
+++ src/components/transform-currency-names/index.js
@@ -4,6 +4,7 @@ import Button from 'preact-material-components/Button';
 import 'preact-material-components/Button/style.css';
 import 'preact-material-components/Dialog/style.css';
 import Table from '../table';
+import styles from './style.less';
 
 export default class TransformCurrencyNames extends Component {
   constructor(props) {
@@ -38,25 +39,30 @@ export default class TransformCurrencyNames extends Component {
   }
 
   render() {
+    const convertSuggestions = this.props.matches.filter(match => !!match.suggestion);
+
     return (
       <Dialog ref={this.setElemRef} onAccept={this.props.onClose}>
         <Dialog.Header>Hantera felaktiga valutanamn</Dialog.Header>
         <Dialog.Body>
           <p>Alla kryptoplånböcker samt växlingssidor tillhandahåller tyvärr inte CSV-filer i maskinärt avläsbara format.</p>
 
-          <Table>
+          {convertSuggestions.length && (
+            <p>Nedan har vi tagit fram förslag på förändring av valutanamnen, gå igenom dem och ändra de som stämmer.</p>
+          )}
+
+          <Table className={styles.Center}>
             <tr>
               <th>Originalvärde</th>
               <th>Föreslaget värde</th>
-              <th>Acceptera förändring</th>
+              <th></th>
             </tr>
 
-            {this.props.matches
-                .filter(match => !!match.suggestion)
+            {convertSuggestions
                 .map(match => ({
                   ...match,
                   disabled: this.props.transformed.indexOf(match.original) > -1 || this.props.currentlyTransforming === match.original,
-                  label: this.props.currentlyTransforming === match.original ? 'Laddar' : 'Acceptera',
+                  label: this.props.currentlyTransforming === match.original ? 'Laddar' : 'Ändra',
                 }))
                 .map(match => (
                   <tr>
